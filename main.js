9359c45d3078aca2641a910e9d8ebb679c0ba736
SteveMelville <43118615+SteveMelville@users.noreply.github.com>
2018-11-05 11:51:54 -0700

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function printBoard(){
     for(var i = 0; i < height; i++){
         boardHTML += "<tr>";
         for(var j = 0; j < width; j++){
-            boardHTML += "<td id = " + (i * width + j) + "></td>";
+            boardHTML += "<td id = " + (i * width + j) + ">hello</td>";
         }
         boardHTML += "</tr>";
     }
