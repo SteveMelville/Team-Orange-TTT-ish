6d86d5bfdc95d0b00ba7b18fd6cf86014b1b1d7e
SteveMelville <43118615+SteveMelville@users.noreply.github.com>
2018-11-05 11:50:52 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+var width;
+var height;
+
+var board = [];
+
+var boardHTML = "";
+var boardAddress = document.getElementById("board");
+
+printBoard();
+
+function printBoard(){
+    for(var i = 0; i < height; i++){
+        boardHTML += "<tr>";
+        for(var j = 0; j < width; j++){
+            boardHTML += "<td id = " + (i * width + j) + "></td>";
+        }
+        boardHTML += "</tr>";
+    }
+    boardAddress.innerHTML += boardHTML;
+}
+
 
 
 
