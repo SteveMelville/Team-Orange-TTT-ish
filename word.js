808b8ab15a0697ef8f94ef1d59ed9a676c7ff485
Matthew <Athewmay@Live.com>
2018-11-12 12:40:17 -0700

    added the dictionary class

---
+++ word.js
@@ -0,0 +1,15 @@
+//a false word class for use in testing the dictionary
+class Word{
+	setName(name){
+		this.name=name;
+	}
+	setPoint(point){
+		this.points=point;
+	}
+	getName(){
+		return this.name;
+	}
+	getPoint(){
+		return this.points;
+	}
+}
