afee9acda0904d0ffeab8e334388b59fbae370cb
Matthew <Athewmay@Live.com>
2018-11-12 15:00:05 -0700

    fixed an error i have been having

--- networkingServer.js
+++ networkingServer.js
@@ -11,6 +11,9 @@ var server = http.createServer(function (req, res) {
 	if (url=="/"){
 		url="/index.html";
 	}
+	if (url.indexOf('favicon.ico')!=-1){
+		return;
+	}
 	//write the head
 	fs.readFile(__dirname + url, function (err, data) {
 		//if there is an error reading the file
