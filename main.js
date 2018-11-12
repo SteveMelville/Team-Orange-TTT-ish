3a992dd8bd4e84f75e6aade3ab5eaa8d13433645
SteveMelville <43118615+SteveMelville@users.noreply.github.com>
2018-11-12 11:58:28 -0700

    Update main.js

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ class board{
                 this.boardHTML += "<td id = '" + id + "'>";
                 
                 switch(this.boardArray[id]){
-                    case 0: this.boardHTML += "<button type='button' onclick='this.pushButton(" + id + ")'>" + this.blank + "</button>";
+                    case 0: this.boardHTML += "<button type='button' onclick='board.pushButton(" + id + ")'>" + this.blank + "</button>";
                             break;
                     case 1: this.boardHTML += this.player1;
                             break;
