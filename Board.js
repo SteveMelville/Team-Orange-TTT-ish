33fecb2995937af5064ebba33b6b96fce5a9dd83
SteveMelville <43118615+SteveMelville@users.noreply.github.com>
2018-11-16 12:10:16 -0700

    Update Board.js

--- Board.js
+++ Board.js
@@ -1,9 +1,23 @@
 const player1 = "<img src='assets/testX.png'>";
 const player2 = "<img src='assets/testO.png'>";
+const player3 = "<img src='assets/'>";
+const player4 = "<img src='assets/'>";
+const player5 = "<img src='assets/'>";
+const player6 = "<img src='assets/'>";
+const player7 = "<img src='assets/'>";
+const player8 = "<img src='assets/'>";
+const player9 = "<img src='assets/'>";
+const player10 = "<img src='assets/'>";
+const player11 = "<img src='assets/'>";
+const player12 = "<img src='assets/'>";
+const player13 = "<img src='assets/'>";
+const player14 = "<img src='assets/'>";
+const player15 = "<img src='assets/'>";
 const blank = "<img src='assets/testBlank.png'>";
-const numPlayers = 2;
 
-var turn = 0;
+
+var numPlayers = 2;
+var turn = 1;
 
 class board{
     
@@ -63,28 +77,55 @@ class board{
     }
     
     get(x, y){
-        if(x > this.width || y > this.height){
+        if(x > this.width || y > this.height || y < 0 || x < 0){
             return null;
         }
         return this.boardArray[(this.width * y) + x];
     }
 }
 
+
+
 function pushButton(id){
     var item = document.getElementById(id);
     
     switch(turn){
-        case 0: item.innerHTML = player1;
+        case 1: item.innerHTML = player1;
                 break;
-        case 1: item.innerHTML = player2;
+        case 2: item.innerHTML = player2;
                 break;
-        
+        case 3:  item.innerHTML = player3;
+                break;
+        case 4:  item.innerHTML = player4;
+                break;
+        case 5:  item.innerHTML = player5;
+                break;
+        case 6:  item.innerHTML = player6;
+                break;
+        case 7:  item.innerHTML = player7;
+                break;
+        case 8:  item.innerHTML = player8;
+                break;
+        case 9:  item.innerHTML = player9;
+                break;
+        case 10:  item.innerHTML = player10;
+                break;
+        case 11:  item.innerHTML = player11;
+                break;
+        case 12:  item.innerHTML = player12;
+                break;
+        case 13:  item.innerHTML = player13;
+                break;
+        case 14:  item.innerHTML = player14;
+                break;
+        case 15:  item.innerHTML = player15;
+                break; 
     }
-    if(turn < numPlayers - 1){
+    if(turn < numPlayers){
         turn++;
     }
     else{
-        turn = 0;
+        turn = 1;
     }
 }
 
