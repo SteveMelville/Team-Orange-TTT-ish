8570ef9c274ac310288af2821be6f209f2f39c92
SteveMelville <43118615+SteveMelville@users.noreply.github.com>
2018-11-19 13:18:53 -0700

    Update Game.js

--- Game.js
+++ Game.js
@@ -39,7 +39,7 @@ class game{
 	}
 	
 	addPlayer(nickname, image){
-		this.players.push(new Player(++numPlayers_, nickname, image));	
+		this.players.push(new Player(++numPlayers, nickname, image));	
 	}
 			    
 	removePlayer(id){
