3e817add732fc164478ad6b50063dd56918dfee0
SteveMelville <43118615+SteveMelville@users.noreply.github.com>
2018-11-19 12:32:35 -0700

    Update Player.js

--- Player.js
+++ Player.js
@@ -1,1 +1,28 @@
 
+class Player{
+  constructor(id, nickname, image){
+      this.id_ = id;
+      this.nickname_ = nickname;
+      this.image_ = image;
+      this.score_ = 0;
+  }
+  
+  getId(){
+      return id_;
+    }
+  
+  getNickname(){
+      return nickname_;
+    }
+  getImage(){
+      return image_;
+  }
+  
+  getScore(){
+      return score_;
+  }
+  
+  setScore(score){
+      score_ = score;
+  }
+}
