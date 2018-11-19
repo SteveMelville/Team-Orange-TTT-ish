d1732897a55e5ff5c5e0527091fb391695a94d35
SteveMelville <43118615+SteveMelville@users.noreply.github.com>
2018-11-19 12:04:27 -0700

    Update Board.js

--- Board.js
+++ Board.js
@@ -1,19 +1,51 @@
-const player1 = "<img src='assets/testX.png'>";
-const player2 = "<img src='assets/testO.png'>";
-const player3 = "<img src='assets/A.png'>";
-const player4 = "<img src='assets/B.png'>";
-const player5 = "<img src='assets/C.png'>";
-const player6 = "<img src='assets/D.png'>";
-const player7 = "<img src='assets/E.png'>";
-const player8 = "<img src='assets/.png'>";
-const player9 = "<img src='assets/.png'>";
-const player10 = "<img src='assets/.png'>";
-const player11 = "<img src='assets/.png'>";
-const player12 = "<img src='assets/.png'>";
-const player13 = "<img src='assets/.png'>";
-const player14 = "<img src='assets/.png'>";
-const player15 = "<img src='assets/.png'>";
+const letterA = "<img src='assets/A.png'>";
+const letterB = "<img src='assets/B.png'>";
+const letterC = "<img src='assets/C.png'>";
+const letterD = "<img src='assets/D.png'>";
+const letterE = "<img src='assets/E.png'>";
+const letterF = "<img src='assets/.png'>";
+const letterG = "<img src='assets/.png'>";
+const letterH = "<img src='assets/.png'>";
+const letterI = "<img src='assets/.png'>";
+const letterJ = "<img src='assets/.png'>";
+const letterK = "<img src='assets/.png'>";
+const letterL = "<img src='assets/.png'>";
+const letterM = "<img src='assets/.png'>";
+const letterN = "<img src='assets/.png'>";
+const letterO = "<img src='assets/.png'>";
+const letterP = "<img src='assets/.png'>";
+const letterQ = "<img src='assets/.png'>";
+const letterR = "<img src='assets/.png'>";
+const letterS = "<img src='assets/.png'>";
+const letterT = "<img src='assets/.png'>";
+const letterU = "<img src='assets/.png'>";
+const letterV = "<img src='assets/.png'>";
+const letterW = "<img src='assets/.png'>";
+const letterX = "<img src='assets/.png'>";
+const letterY = "<img src='assets/.png'>";
+const letterZ = "<img src='assets/.png'>";
+
+const testX = "<img src='assets/testX.png'>";
+const testO = "<img src='assets/testO.png'>";
 const blank = ""; //"<img src='assets/testBlank.png'>";
+                                      
+
+var player1 = textX;
+var player2 = testO;
+var player3 = letterA;
+var player4 = letterB;
+var player5 = letterC;
+var player6 = letterD;
+var player7 = letterE;
+var player8 = letterF;
+var player9 = letterG;
+var player10 = letterH;
+var player11 = letterI;
+var player12 = letterJ;
+var player13 = letterK;
+var player14 = letterL;
+var player15 = letterM;
+
 
 
 var numPlayers = 7;
