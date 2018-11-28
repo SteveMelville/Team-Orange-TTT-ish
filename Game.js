cdcbebbf63b0b3bd53bdee0a44906322fba1d368
Matthew <Athewmay@Live.com>
2018-11-28 12:02:01 -0700

    fixed for merge

--- Game.js
+++ Game.js
@@ -41,7 +41,7 @@ class game{
 	}
 	getFirstPlacePlayer(){
 		winner = this.players[0]
-		for(i=1;i<this.players.length;i++){
+		for(i=1;i < this.players.length;i++){
 			if(this.players[i].getScore>winner.getScore){
 				winner=this.players[i];
 			}
@@ -54,9 +54,10 @@ class game{
 	}
 			    
 	removePlayer(id){
-		for(var i=0;i<players.length();i++){
+		for(var i = 0;i < this.players.length; i++){
 			if(players[i].getID()==id){
 				this.players.splice(i,1);
+				this.numPlayers--;
 				return ;
 			}
 		}
@@ -75,16 +76,82 @@ class game{
 			this.turn = 1;
 		}
 	}
+	
+	printLeaderboard(){
+		var leaderBoard = "";
+		for(var i = 0; i < this.numPlayers; i++){
+			leaderBoard += leader1;
+			leaderBoard += i;
+			leaderBoard += leader2;
+			
+			leaderBoard += leader3;
+			leaderBoard += i + "id";
+			leaderBoard += leader2;
+			leaderBoard += i + 1;
+			leaderBoard += leader4;
+			
+			leaderBoard += leader3;
+			leaderBoard += i + "image";
+			leaderBoard += leader2;
+			leaderBoard += this.players[i].getImage();
+			leaderBoard += leader4;
+			
+			leaderBoard += leader3;
+			leaderBoard += i + "name";
+			leaderBoard += leader2;
+			leaderBoard += this.players[i].getNickname();
+			leaderBoard += leader4;
+			
+			leaderBoard += leader3;
+			leaderBoard += i + "score";
+			leaderBoard += leader2;
+			leaderBoard += this.players[i].getScore();
+			leaderBoard += leader4;
+			
+			leaderBoard += leader5;
+		}
+		var item = document.getElementById("leader");
+		item.innerHTML = leaderBoard;
+	}
+	
+	printBoard(){
+		var id = 0;
+		var boardHTML = "";
+		for(var i = 0; i < this.board.height; i++){
+		    boardHTML += "<tr>\n";
+		    for(var j = 0; j < this.board.width; j++){
+			id = i * this.board.width + j;
+			boardHTML += "<td id = '" + id + "'>";
+
+			switch(this.board.boardArray[id]){
+			    case 0: boardHTML += "<button type='button' onclick='pushButton(" + id + ")'>" + blank + "</button>";
+				    break;
+			    case 1: boardHTML += player1;
+				    break;
+			    default: break;
+			}
+
+			boardHTML += "</td>\n";
+		    }
+
+		    boardHTML += "</tr>\n";
+		}
+		
+        	var boardAddress = document.getElementById("board");
+		boardAddress.innerHTML = boardHTML;
+    	}
 }
 
-var newGame = new game(15,15);
-newGame.board.printBoard();
+var newGame = new game(3,3);
+newGame.printBoard();
+newGame.printLeaderboard();
 	
 function pushButton(id){
-    var item = document.getElementById(id);
-    
-    item.innerHTML = newGame.players[newGame.getTurn() - 1].getImage();
-    scanBoard(newGame, id);
-    newGame.board.set(id, newGame.getTurn());
-    newGame.updateTurn();
+	var item = document.getElementById(id);
+
+	item.innerHTML = newGame.players[newGame.getTurn() - 1].getImage();
+	scanBoard(newGame, id);
+	newGame.board.set(id, newGame.getTurn());
+	newGame.updateTurn();
+	newGame.printLeaderboard();
 }
