346214b66e24536e079d62d3c492656cd8e7357c
SteveMelville <43118615+SteveMelville@users.noreply.github.com>
2018-11-28 12:40:30 -0700

    Update Game.js

--- Game.js
+++ Game.js
@@ -41,7 +41,8 @@ class game{
 	checkWin(player){
 		if(player.getScore()>=this.PointsToWin){
 			
-			var buttons = document.getElementsByTagName("TD").contains("BUTTON");
+			var boardState = document.getElementById("board");
+			var buttons = board.getElementsByTagName("TD").contains("BUTTON");
 			for(e in buttons){
 				buttons[e].innerHTML = blank;
 			}
