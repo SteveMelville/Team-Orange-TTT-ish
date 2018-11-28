e959dc4ea4c825897a68deee77b5b58aedd7b73d
SteveMelville <43118615+SteveMelville@users.noreply.github.com>
2018-11-28 12:33:37 -0700

    Update Game.js

--- Game.js
+++ Game.js
@@ -135,7 +135,7 @@ class game{
 			boardHTML += "<td id = '" + id + "'>";
 
 			switch(this.board.boardArray[id]){
-			    case 0: boardHTML += "<button type='button' onclick='pushButton(" + id + ")'>" + blank + "</button>";
+			    case 0: boardHTML += "<button type='button' onclick='pushButton(" + id + ")'></button>";
 				    break;
 			    case 1: boardHTML += player1;
 				    break;
