927c7208f405b75f9e13e949bd84f9a09de0e877
SirNodnarb <43394345+SirNodnarb@users.noreply.github.com>
2018-11-30 10:58:21 -0700

    Update Game.js

--- Game.js
+++ Game.js
@@ -170,7 +170,8 @@ function pushButton(id){
 	newGame.printLeaderboard();
 	newGame.gameOver();
 	if(newGame.checkWin(newGame.players[newGame.getTurn() - 1])){
-		lockBoard(newGame);	
+		lockBoard(newGame);
+		alert(newGame.players[newGame.getTurn() - 1] + ' has won the game!');
 	}	
 	newGame.updateTurn();
 }
