684e31c1ee9d7c92d849d10e2aeb7118b93833cc
TheDarkeOne <ammonzerkle@gmail.com>
2018-12-03 11:51:26 -0700

    Better comments

--- Player.js
+++ Player.js
@@ -3,40 +3,30 @@ class Player{
   
   //Constructor for the player class that instantiates the id_, nickname_ and image_ to the values passed to it and score_ to a default value
   constructor(id, nickname, image){
-      //Assigns the id passed to the constructor to id_
       this.id_ = id;
-    
-      //Assigns the nickname passed to the constructor to nickname_
       this.nickname_ = nickname;
-    
-     //Assigns the image passed to the constructor to image_
       this.image_ = image;
-    
-      //Assigns 0 to the score_ 
       this.score_ = 0;
   }
   
+  //Get functions that return the player values
   getId(){
-      //returns the value in id_
       return this.id_;
     }
   
   getNickname(){
-      //returns the value in nickname_
       return this.nickname_;
     }
   getImage(){
-      //returns the value in image_
       return this.image_;
   }
   
   getScore(){
-      //returns the value in score_
       return this.score_;
   }
   
+  //Sets the score to a new value
   setScore(score){
-      //Sets the value of id_ to the value passed to the function
       this.score_ = score;
   }
 }
